19f160bdb947b5ca302ea5dd9b36cfd003377597
V1olenceDev <gasparyan.valerik@gmail.com>
2023-11-28 23:10:46 +0500

    Test v5

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -3,10 +3,9 @@ from django.core import exceptions as django_exceptions
 from django.db import transaction
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from drf_base64.fields import Base64ImageField
-from rest_framework import serializers
-
 from recipes.models import (Favorite, Ingredient, Recipe, Recipe_ingredient,
                             Shopping_cart, Tag)
+from rest_framework import serializers
 from users.models import Subscribe, User
 
 
--- backend/recipes/managment/commands/import_data.py
+++ backend/recipes/managment/commands/import_data.py
@@ -1,9 +1,7 @@
 import csv
 
 from django.core.management.base import BaseCommand
-
 from recipes.models import Ingredient
-
 
 PATH = 'data/'
 
