73157794c4a3186a1e78d274e2e3893e2475fd72
V1olenceDev <gasparyan.valerik@gmail.com>
2023-12-11 16:50:45 +0500

    Тест

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -12,6 +12,6 @@ router.register('ingredients', views.FoodIngredientViewSet,
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('auth/', include('djoser.urls')),
+    path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
