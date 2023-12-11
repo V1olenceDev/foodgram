046d3514ab64068663a2ac1efd7f05b0b04e0828
V1olenceDev <gasparyan.valerik@gmail.com>
2023-12-11 15:43:09 +0500

    Тест

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,11 +6,12 @@ from . import views
 router = DefaultRouter()
 router.register('recipes', views.RecipeViewSet, basename='recipes')
 router.register('tags', views.TagViewSet, basename='tags')
-router.register('user', views.UserProfileViewSet, basename='user')
+router.register('users', views.UserProfileViewSet, basename='users')
 router.register('ingredients', views.FoodIngredientViewSet,
                 basename='ingredients')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
